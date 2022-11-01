e228585e0f96c1797a8429c2460817fe4f457558
Shobhit4225 <91666201+Shobhit4225@users.noreply.github.com>
2022-11-01 07:26:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const UserModel = require('./models/Users')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const cors = require('cors')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://shobhit:Mumbai123@cluster0.sk4vejq.mongodb.net/merntutorial?retryWrites=true&w=majority")
+
+app.get("/getUsers" , (req , res)=>{
+    UserModel.find({} , (err , result)=> {
+        if(err){
+            res.json(err);
+        }else{
+            res.json(result)
+        }
+    });
+});
+
+app.post("/createUser" , async (req , res)=>{
+    const user = req.body
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+})
+
+// tell api to start
+app.listen(3001 , () =>{
+    console.log("SERVER RUNS PERFECTLY!") 
+})
